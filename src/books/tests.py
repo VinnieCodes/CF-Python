25c68d9aeb5a8fe94e5001962df8bbfa4f9d3679
Vinnie <vincentathorne2005@gmail.com>
2024-11-04 14:26:12 -0800

    2.3

--- src/books/tests.py
+++ src/books/tests.py
@@ -7,7 +7,7 @@ class BookModelTest(TestCase):
 
   def setUpTestData():
     # Set up non-modified objects used by all test methods
-    Book.objects.create(name = 'Pride and Prejudice', author_name = 'Jane Austen', genre = 'classic', book_type = 'hardcover', price = '23.71')
+    Book.objects.create(name = 'Pride and Prejudice', genre = 'classic', book_type = 'hardcover', price = '23.71')
     
   def test_book_name(self):
     # Get a book object to test
@@ -21,7 +21,7 @@ class BookModelTest(TestCase):
     # Get a book object to test
     book = Book.objects.get(id=1)
     # Get the metadata for the 'author_name' field and use it to query its max_length
-    max_length = book._meta.get_field('author_name').max_length
+    max_length = book._meta.get_field('genre').max_length
     # Compare the value to the expected result i.e. 120
-    self.assertEqual(max_length, 120)
+    self.assertEqual(max_length, 12)
     
