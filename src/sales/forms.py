a0178e5e66a446370cb1c5e0429c5e3c2db17868
Vinnie <vincentathorne2005@gmail.com>
2024-11-28 10:05:35 -0800

    first form

---
+++ src/sales/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+CHART__CHOICES = (
+  ('#1', 'Bar Chart'),
+  ('#2', 'Pie Chart'),
+  ('#3', 'Line Chart'),
+)
+
+class SalesSearchForm(forms.Form):
+  book_title = forms.CharField(max_length = 120)
+  chart_type = forms.ChoiceField(choices = CHART__CHOICES)
--- src/sales/views.py
+++ src/sales/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import SalesSearchForm
 
 # Create your views here.
 
@@ -6,4 +7,9 @@ def home(request):
   return render(request, 'sales/home.html')
 
 def records(request):
+  form = SalesSearchForm(request.POST or None)
+  context = {
+    'form': form
+  }
+  
   return render(request, 'sales/records.html')
