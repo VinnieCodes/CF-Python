b49ed3d2831b833e539ca9402da548ee6b797715
Vinnie <vincentathorne2005@gmail.com>
2024-11-30 10:21:30 -0800

    form

--- src/sales/views.py
+++ src/sales/views.py
@@ -8,8 +8,16 @@ def home(request):
 
 def records(request):
   form = SalesSearchForm(request.POST or None)
+  if request.method == 'POST':
+    # read book_title and chart_type
+    book_title = request.POST.get('book_title')
+    chart_type = request.POST.get('chart_type')
+    # display in terminal - needed for debugging during development only
+    print(book_title, chart_type)
+
+  # pack up data to be sent to template in the context dictionary
   context = {
-    'form': form
+    'form': form,
   }
-  
-  return render(request, 'sales/records.html')
+
+  return render(request, 'sales/records.html', context)
