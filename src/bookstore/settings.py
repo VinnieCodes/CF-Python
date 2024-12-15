c37c01ac7d341bec7c127137932b2c7d92c9b8c2
Vinnie <vincentathorne2005@gmail.com>
2024-12-15 13:35:52 -0800

    deploy to heroku

--- src/bookstore/settings.py
+++ src/bookstore/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = "static/"
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/media/" 
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
